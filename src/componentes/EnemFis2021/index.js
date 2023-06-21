d8476e271491199f7e5deebdcf5c17a0f7da969a
Ricardow3 <ricardobrazleao@gmail.com>
2023-06-21 19:27:20 -0300

    another solution on EnemFis2021, updated readme

--- src/componentes/EnemFis2021/index.js
+++ src/componentes/EnemFis2021/index.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import Header from "../Header";
 import Aside from "../Aside";
-import Q92_EnemFis2021 from "./Q92";
-import Q94_EnemFis2021 from "./Q94";
-import Q105_EnemFis2021 from "./Q105";
-import Q107_EnemFis2021 from "./Q107";
-import Q115_EnemFis2021 from "./Q115";
-import Q126_EnemFis2021 from "./Q126";
 
 const EnemFis2021 = () => {
 
     const Pages = {
-        q92: Q92_EnemFis2021,
-        q94: Q94_EnemFis2021,
-        q105: Q105_EnemFis2021,
-        q107: Q107_EnemFis2021,
-        q115: Q115_EnemFis2021,
-        q126: Q126_EnemFis2021
+        q92: "./Q92",
+        q94: "./Q94",
+        q105: "./Q105",
+        q107: "./Q107",
+        q115: "./Q115",
+        q126: "./Q126"
     }
 
     const [newPage, setnewPage] = useState("q92");
@@ -24,6 +18,8 @@ const EnemFis2021 = () => {
     const newpageFunction = (pagina) => {
         setnewPage(pagina);
     }
+
+    const PageComponent = lazy(() => import(`${Pages[newPage]}`));
 
     return (
         <div>
@@ -36,12 +32,9 @@ const EnemFis2021 = () => {
             name = 'Questão 92' number = {92}
             listQ = {[94, 105, 107, 115, 126]} />
 
-            {newPage === "q92" && <Pages.q92 />}
-            {newPage === "q94" && <Pages.q94 />}
-            {newPage === "q105" && <Pages.q105 />}
-            {newPage === "q107" && <Pages.q107 />}
-            {newPage === "q115" && <Pages.q115 />}
-            {newPage === "q126" && <Pages.q126 />}
+            <Suspense>
+                <PageComponent />
+            </Suspense>
         </div>
     )
 }
